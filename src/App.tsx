f45cd33f657d820cf75d64ec7577feb7b6141137
mahavinay <mahavinay28@gmail.com>
2021-06-08 16:28:11 +0200

    crreated interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
+interface IState {
+    people: {
+        name: string;
+        age: number;
+        url: string;
+        notes?: string;
+    }[];
+}
+
 function App() {
+    const [people, setPeople] = useState<IState["people"]>([]);
+
+    people.map((person) => person.age);
     return (
         <div className="App">
             <p>Maha</p>
