37443007bca39a39769ef064443eccb4f73a10ca
mahavinay <mahavinay28@gmail.com>
2021-06-09 17:14:57 +0200

    Added components

---
+++ src/components/AddToList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const AddToList = () => {
+    return (
+        <div className="AddTList">
+            <input type="text" placeholder="name" className="AddToList-input"/>
+            <input type="text" placeholder="Age" className="AddToList-input"/>
+            <input type="text" placeholder="Image" className="AddToList-input"/>
+            <textarea  placeholder="Image" className="AddToList-input"/>
+            "
+        </div>
+    )
+}
